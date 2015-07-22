6ef13e6e9acaea38d15e68990322dd813dd454ba
Hein Bekker <hein@netbek.co.za>
2015-07-22 17:52:44 +0200

    Allow for sitemap to be defined in config. Clean up formatting.

--- README.md
+++ README.md
@@ -16,9 +16,16 @@ Create `config.js`
 
 ```javascript
 var benderConfig = {
+	// Option 1: Define sitemap
+	sitemap: {
+		'url': 'title',
+		'another/url': 'another title'
+	},
+	// Option 2: Load sitemap from file (set `sitemapUrl`, `sitemapBaseUrl`, `baseUrl`)
 	sitemapUrl: 'http://example.com/sitemap.xml',
 	sitemapBaseUrl: 'http://example.com',
 	baseUrl: '',
+	//
 	screens: [
 		'320-480',
 		'768-1024',
--- src/js/bender.js
+++ src/js/bender.js
@@ -12,7 +12,16 @@
 		return;
 	}
 
-	function Bender () {
+	/**
+	 *
+	 * @returns {Bender}
+	 */
+	function Bender() {
+		this.flags = {
+			init: false
+		};
+
+		this.config = {};
 		this.ratios = [];
 
 		// Build list of recognised ratios.
@@ -20,7 +29,7 @@
 			[10, 16],
 			[10, 15],
 			[9, 16],
-			[3, 4],
+			[3, 4]
 		];
 		var value;
 		for (var i = 0, il = arr.length; i < il; i++) {
@@ -44,9 +53,9 @@
 
 	/**
 	 *
-	 * @param {int} width
-	 * @param {int} height
-	 * @returns {string}
+	 * @param {Number} width
+	 * @param {Number} height
+	 * @returns {String}
 	 */
 	Bender.prototype.getRatioName = function (width, height) {
 		var ratio = Number(width) / Number(height), value;
@@ -61,103 +70,132 @@
 
 	/**
 	 *
-	 * @param {object} config
+	 * @param {Object} options
 	 */
-	Bender.prototype.init = function (config) {
-		config = jQuery.extend({}, {
+	Bender.prototype.init = function (options) {
+		if (this.flags.init) {
+			return;
+		}
+
+		var self = this;
+
+		this.flags.init = true;
+
+		this.config = jQuery.extend({}, {
+			sitemap: undefined,
 			sitemapUrl: undefined,
 			sitemapBaseUrl: undefined,
 			baseUrl: '',
 			screens: []
-		}, config);
+		}, options);
 
 		// Append iframes.
 		var $ul = jQuery('#frames > ul');
 		var screen, dims, ratioName;
 		var html = '';
-		for (var i = 0, il = config.screens.length; i < il; i++) {
-			screen = config.screens[i];
+		for (var i = 0, il = this.config.screens.length; i < il; i++) {
+			screen = this.config.screens[i];
 			dims = screen.split('-');
-			ratioName = bender.getRatioName(dims[0], dims[1]);
+			ratioName = self.getRatioName(dims[0], dims[1]);
 			html += '<li><p>' + dims[0] + ' x ' + dims[1] + (ratioName ? ', ' + ratioName : '') + '</p><iframe class="f f-' + dims[0] + '-' + dims[1] + '"></iframe></li>';
 		}
 		$ul.append(html);
 
-		// Load sitemap.
-		jQuery.ajax({
-			cache: false,
-			method: 'GET',
-			dataType: 'xml',
-			url: config.sitemapUrl,
-			error: function () {
-				alert('Could not load sitemap');
-			},
-			success: function (data, textStatus, jqXHR) {
-				var $frames = jQuery('iframe');
-				var $sfElm = jQuery('#menu');
-				var $listElm = jQuery('<ul />');
-				var urls = {};
+		if (this.config.sitemap !== undefined) {
+			// Load frames.
+			this.loadFrames();
+		}
+		else if (this.config.sitemapUrl !== undefined) {
+			// Load sitemap.
+			jQuery.ajax({
+				cache: false,
+				method: 'GET',
+				dataType: 'xml',
+				url: this.config.sitemapUrl,
+				error: function () {
+					throw new Error('Could not load sitemap.');
+				},
+				success: function (data, textStatus, jqXHR) {
+					self.config.sitemap = {};
 
-				jQuery(data).find('url').each(function () {
-					var url = jQuery(this).find('loc').text();
+					jQuery(data).find('url').each(function () {
+						var url = jQuery(this).find('loc').text();
 
-					if (url.substr(0, config.sitemapBaseUrl.length) === config.sitemapBaseUrl) {
-						var title = url = url.substr(config.sitemapBaseUrl.length);
+						if (url.substr(0, self.config.sitemapBaseUrl.length) === self.config.sitemapBaseUrl) {
+							var title = url = url.substr(self.config.sitemapBaseUrl.length);
 
-						if (title === '') {
-							title = '/';
+							if (title === '') {
+								title = '/';
+							}
+
+							url = self.config.baseUrl + url;
+							self.config.sitemap[url] = title;
 						}
-
-						url = config.baseUrl + url;
-						urls[url] = title;
-					}
-				});
-
-				var keys = [], k, i, len;
-
-				for (k in urls) {
-					if (urls.hasOwnProperty(k)) {
-						keys.push(k);
-					}
-				}
-
-				keys.sort();
-				len = keys.length;
-
-				for (i = 0; i < len; i++) {
-					k = keys[i];
-
-					if (config.firstUrl === undefined) {
-						config.firstUrl = k;
-					}
-
-					var $aElm = jQuery('<a href="' + k + '">' + urls[k] + '</a>').on('click', function () {
-						$frames.attr('src', jQuery(this).attr('href'));
-						return false;
 					});
-					var $itemElm = jQuery('<li />');
-					$itemElm.append($aElm);
-					$listElm.append($itemElm);
+
+					// Load frames.
+					self.loadFrames();
 				}
+			});
+		}
+		else {
+			throw new Error('Either `sitemap` or `sitemapUrl` should be defined in config.');
+		}
+	};
 
-				jQuery('a[href="#"]', $sfElm).on('click', function () {
-					return false;
-				});
+	/**
+	 *
+	 */
+	Bender.prototype.loadFrames = function () {
+		if (!this.flags.init) {
+			return;
+		}
 
-				if (len) {
-					jQuery('li.sitemap', $sfElm).append($listElm);
-					$frames.attr('src', config.firstUrl);
-				}
+		var $frames = jQuery('iframe');
+		var $sfElm = jQuery('#menu');
+		var $listElm = jQuery('<ul />');
+		var keys = [], k, i, len;
 
-				$sfElm.removeClass('hidden').superfish({
-					delay: 100,
-					speed: 100
-				});
+		for (k in this.config.sitemap) {
+			if (this.config.sitemap.hasOwnProperty(k)) {
+				keys.push(k);
 			}
+		}
+
+		keys.sort();
+		len = keys.length;
+
+		for (i = 0; i < len; i++) {
+			k = keys[i];
+
+			if (this.config.firstUrl === undefined) {
+				this.config.firstUrl = k;
+			}
+
+			var $aElm = jQuery('<a href="' + k + '">' + this.config.sitemap[k] + '</a>').on('click', function () {
+				$frames.attr('src', jQuery(this).attr('href'));
+				return false;
+			});
+			var $itemElm = jQuery('<li />');
+			$itemElm.append($aElm);
+			$listElm.append($itemElm);
+		}
+
+		jQuery('a[href="#"]', $sfElm).on('click', function () {
+			return false;
+		});
+
+		if (len) {
+			jQuery('li.sitemap', $sfElm).append($listElm);
+			$frames.attr('src', this.config.firstUrl);
+		}
+
+		$sfElm.removeClass('hidden').superfish({
+			delay: 100,
+			speed: 100
 		});
 	};
 
-	var bender = new Bender;
-	window.bender = bender;
+	window.bender = new Bender();
 
 })(this);
